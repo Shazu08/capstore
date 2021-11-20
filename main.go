b70a624291eb43717af539600abc22f93c32c519
capslock-inc <casplock0408@gmail.com>
2021-11-20 09:52:10 +0530

    just testing pullrequest

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	// fmt.Println("hello")
 
-	fmt.Println("Im in as a collaborator")
+	// fmt.Println("Im in as a collaborator")
+
+	var shazu int
+	var althaf int
+	sum(input(althaf), input(shazu))
+
+}
+func input(t int) int {
+	fmt.Scanf("%d", &t)
+	return t
+}
+func sum(a int, b int) {
+	println(a + b)
 }
